88f9c307ebcc6f14840e6c1370b5e5514827dfe8
Simone <simone.maccio2@gmail.com>
2023-03-06 16:29:40 +0100

    switched to JPA repository

--- src/main/java/com/example/orders/OrderRepository.java
+++ src/main/java/com/example/orders/OrderRepository.java
@@ -1,8 +1,8 @@
 package com.example.orders;
 
-//import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.repository.CrudRepository;
 
-interface OrderRepository extends CrudRepository<Order, Long> {
+interface OrderRepository extends JpaRepository<Order, Long> {
     
 }
